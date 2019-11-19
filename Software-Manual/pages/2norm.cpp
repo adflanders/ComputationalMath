fd83acdf3e619a6d8e23640e3fff00b627c467e4
Adam Flanders <52503503+adflanders@users.noreply.github.com>
2019-11-19 08:01:18 -0700

    Create 2norm.cpp

---
+++ Software-Manual/pages/2norm.cpp
@@ -0,0 +1,16 @@
+# 2-Norm of a vector
+
+- Is the most popular norm, also known as the Euclidean norm. It is the shortest distance to go from one point to another.  
+|x|^2 = |x1|^2 + |x2|^2
+
+## Input
+
+- Just takes a vector of any size.
+
+## Output
+
+- Returns a single double value, which is |x| = sqrt(|x1|^2 + |x2|^2)
+
+## Code
+
+[2-norm](https://github.com/adflanders/math4610/edit/master/src/src4/2norm.cpp)
